65276c2698c5b4e821d05765e8fef1334144cea8
SAG2004 <ghouriabdullah45@gmail.com>
2024-10-31 16:17:22 +0530

    adding files

---
+++ index.js
@@ -0,0 +1,34 @@
+let homePoints = document.getElementById("our")
+let guestPoints = document.getElementById("theirs")
+let count=0
+let coun = 0
+function add1()
+{
+    count+=1
+    homePoints.textContent = count
+}
+function add2()
+{
+    count+=2
+    homePoints.textContent = count
+}
+function add3()
+{
+    count+=3
+    homePoints.textContent = count
+}
+function add4()
+{
+    coun+=1
+    guestPoints.textContent = coun
+}
+function add5()
+{
+    coun+=2
+    guestPoints.textContent = coun
+}
+function add6()
+{
+    coun+=3
+    guestPoints.textContent = coun
+}
